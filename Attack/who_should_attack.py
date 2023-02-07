2e39791078b876c864bd4352c62a966be0f96c96
yoavm <yoav.meron11@gmail.com>
2023-02-07 19:02:39 +0200

    Fixed syntax errors in who_should_attack function.

--- Attack/who_should_attack.py
+++ Attack/who_should_attack.py
@@ -1,17 +1,20 @@
 from penguin_game import *
 
-def calculateDistance(my_iceberg, attack_iceberg):
-    my_iceberg.get_turns_till_arrival(attack_iceberg)
 
-# We might want to make there need to be a certain amount of penguins sent during the attack in case they defend their iceberg
+def calculateDistance(my_iceberg, attack_iceberg):
+    return my_iceberg.get_turns_till_arrival(attack_iceberg)
+
+
+# We might want to make there need to be a certain
+# amount of penguins sent during the attack in case they defend their iceberg
 def who_should_attack(game, attack_iceberg):
     icebergs = game.get_my_icebergs
     icebergs_by_distance = []
-    distances=[]
+    distances = []
     for iceberg in icebergs:
         distance = calculateDistance(iceberg, attack_iceberg)
         distances.append(distance)
-        icebergs_by_distance.append[distance, iceberg]
+        icebergs_by_distance.append((distance, iceberg))
     icebergs_by_distance.sort()
     for iceberg in icebergs_by_distance:
         distance = iceberg[0]
@@ -25,7 +28,9 @@ def who_should_attack(game, attack_iceberg):
                 return icebergs_to_attack
     return None
 
-# If there are multiple icebergs in the same distances, we can send penguins from both at the same time if it's not possible to send penguins from only one and still be able to attack the enemy iceberg
+
+# If there are multiple icebergs in the same distances, we can send penguins from both at
+# the same time if it's not possible to send penguins from only one and still be able to attack the enemy iceberg
 def check_multiple_hits(distance, icebergs_by_distance, attack_iceberg):
     same_distance_icebergs = []
     total_penguin_count = 0
