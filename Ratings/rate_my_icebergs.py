8d5e62a6b247ee84ff24e0183022bd463cf311a1
liabaror <124588094+liabaror@users.noreply.github.com>
2023-02-07 18:35:47 +0200

    Update rate_my_icebergs.py

--- Ratings/rate_my_icebergs.py
+++ Ratings/rate_my_icebergs.py
@@ -24,3 +24,39 @@ def rate_my_attacked_icebergs(attacked_icebergs, game):
             iceberg_rating = rate_iceberg(iceberg, game)
             if iceberg_rating > 20:
                 icebergs_to_be_defended.append(icebergs_to_be_defended)
+                
+ def rate_for_upgrade(iceberg, game):
+    
+    //returns the best iceberg to upgrade
+    
+    icebergs_to_update = []
+    for iceberg in game.get_my_icebergs():
+        
+        distance_from_home = iceberg.get_turns_till_arrival(game.get_my_icepital_icebergs[0])
+        distance_from_enemy = iceberg.get_turns_till_arrival(game.get_enemy_icepital_icebergs[0])
+        per_turn = iceberg.penguins_per_turn
+    
+    //prioritizes upgrading the closest icebergs to our capital or to enemy capital with the least per_turn first
+    //lower score = upgrade first
+    
+        if(distance_from_home < distance_from_enemy)
+            rating = distance_from_home + per_turn
+        else
+            rating = distance_from_enemy + per_turn
+        icebergs_to_update.append(rating)
+     icebergs_to_update.sort()
+    
+    i = 0
+    for iceberg in game.get_my_icebergs():
+        distance_from_home = iceberg.get_turns_till_arrival(game.get_my_icepital_icebergs[0])
+        distance_from_enemy = iceberg.get_turns_till_arrival(game.get_enemy_icepital_icebergs[0])
+        per_turn = iceberg.penguins_per_turn
+        if(distance_from_home < distance_from_enemy)
+            rating = distance_from_home + per_turn
+        else
+            rating = distance_from_enemy + per_turn
+            
+        if rating == icebergs_to_update[i]:
+            return iceberg
+        i++
+    return null
