48f5bce76db47a4fbacc0a8d3b3cbb22434a057d
yoavm <yoav.meron11@gmail.com>
2023-02-07 19:04:23 +0200

    Fixed syntax errors in check_if_c_safe function and moved to Defense Directory.

--- Defense/check_if_c_safe.py
+++ Defense/check_if_c_safe.py
@@ -1,14 +1,15 @@
 from penguin_game import *
 
-# Checks for each group of enemy penguins if 1) They are attacking the Icepital, 2) If the number of penguins in the group is less than the number of penguins currently on the iceberg + the amount of penguins that will be added by the time they arrive + 1
-def check_if_C_safe(self, enemy_groups):
+
+# Checks for each group of enemy penguins if 1) They are attacking the Icepital,
+# 2) If the number of penguins in the group is less than the number of
+# penguins currently on the iceberg + the amount of penguins that will be added by the time they arrive + 1
+def check_if_C_safe(enemy_groups):
     for enemy_group in enemy_groups:
-        if enemy_group.destination.is_icepital == True:
+        if enemy_group.destination.is_icepital:
             target = enemy_group.destination
             turns_till_arrival = enemy_group.turns_till_arrival
             additional_penguins = target.penguins_per_turn * turns_till_arrival
             if enemy_group.penguin_amount < (target.penguin_amount + additional_penguins + 1):
                 return True
     return False
-
-    
