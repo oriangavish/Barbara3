06a3fbfaa19615055b4250a45591a23f4f2306f6
yoavm <yoav.meron11@gmail.com>
2023-02-08 20:06:20 +0200

    Deleted old code.

--- Defense/check_who_should_send.py
+++ Defense/check_who_should_send.py
@@ -5,15 +5,7 @@ from send_able import send_able_defense
 # Function returns list of surrounding icebergs that should send help.
 # If needed it should ask for help from all our icebergs.
 def check_who_should_send_to_capital(game, penguin_group):
-    my_icebergs = game.get_my_icebergs()
-    amount_acquired = 0
-    counter = 0
-    while True:
-        sent = send_able_defense(my_icebergs[counter], penguin_group)
-        if sent:
-            break
-        else:
-            counter += 1
+    return
 
 
 # Function returns list of surrounding icebergs that should send help. Maximum helpers should be 2 for now.
