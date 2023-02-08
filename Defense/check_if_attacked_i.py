e471160ae4137123b82e5b083b4c4a58c2c22844
yoavm <yoav.meron11@gmail.com>
2023-02-08 19:02:22 +0200

    Added function to find all attacked icebergs

--- Defense/check_if_attacked_i.py
+++ Defense/check_if_attacked_i.py
@@ -1,10 +1,18 @@
-
 def check_if_attacked_i(iceberg, game):
+    # returns True if iceberg attacked and False if not
 
-# returns True if iceberg attacked and False if not
+    for enemy in game.get_enemy_penguin_groups():
+        if enemy.destination == iceberg:
+            return True
+        else:
+            return False
 
-  for enemy in game.get_enemy_penguin_groups():
-    if enemy.destination == iceberg:
-      return True
-    else:
-      return False
+
+def find_all_attacked_icebergs(game):
+    # Checks all our icebergs to see if attacked
+    attacked_icebergs = []
+    for iceberg in game.get_my_icebergs():
+        if check_if_attacked_i(iceberg, game):
+            attacked_icebergs.append(iceberg)
+
+    return attacked_icebergs
